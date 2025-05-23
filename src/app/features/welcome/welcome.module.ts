1fbf9682945dfe0329aba65278760cc8f84b250e
nilabhsubramaniam <nilabhsubramaniam@gmail.com>
2025-05-24 00:58:33 +0530

    login screena and ux enhancement for the welcome module

--- src/app/features/welcome/welcome.module.ts
+++ src/app/features/welcome/welcome.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeRoutingModule } from './welcome-routing.module';
-import { WelcomeComponent } from './welcome/welcome.component';
 import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
     CommonModule,
     MatIconModule, // Added MatIconModule to resolve 'mat-icon' error
-    WelcomeRoutingModule,
-    WelcomeComponent
+    WelcomeRoutingModule
   ]
 })
 export class WelcomeModule {}
