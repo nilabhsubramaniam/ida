524fdea844aea3ec5f774f82f819dd44c489ccd8
nilabhsubramaniam <nilabhsubramaniam@gmail.com>
2025-06-01 14:43:56 +0530

    fix angular deployment along with theme rectification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,28 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'vyaktigatavRtta';
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    // Handle redirect parameter for GitHub Pages
+    this.route.queryParams.subscribe(params => {
+      const redirectTo = params['redirect'];
+      if (redirectTo) {
+        // Remove leading/trailing slashes and clean up the path
+        const cleanPath = redirectTo.replace(/^\/+|\/+$/g, '');
+        // Navigate without redirect parameter to prevent loops
+        this.router.navigate(['/' + cleanPath], {
+          replaceUrl: true,
+          queryParamsHandling: 'merge',
+          queryParams: { redirect: null }
+        });
+      }
+    });
+  }
 }
